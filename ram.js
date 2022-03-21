e954c195f3c55bc8d533b0efe2014c6dc87e512a
Dj911 <dharmaraj.jadeja@solulab.co>
2022-03-21 11:08:06 +0530

    Ramp webhook test

---
+++ ram.js
@@ -0,0 +1,20 @@
+import express from "express"
+import bodyParser from "body-parser"
+const app = express();
+
+app.use(bodyParser.json());
+
+app.post('/', function (request, response) {
+    console.log('HERE!!')
+    const event = request.body;
+    console.log(event);
+    const purchase = event.purchase;
+
+    console.log(purchase.id);
+    console.log(purchase.fiatValue);
+    console.log(purchase.fiatCurrency);
+
+    response.send('OK');
+});
+
+app.listen(3000);
