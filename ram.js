328b271894edf7db2a93167a8ee76711c570f89e
Dharmaraj Jadeja <dharmaraj.jadeja@solulab.co>
2022-03-21 15:52:17 +0530

    Update ram.js

--- ram.js
+++ ram.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(bodyParser.json());
 
-const rampKey = readFileSync(`${path.resolve()}/ramp-public-test.pem).toString();
+const rampKey = readFileSync(`${path.resolve()}/ramp-public-test.pem`).toString();
 
 app.post('/webHook', function (request, response) {
     console.log('HERE!!')
